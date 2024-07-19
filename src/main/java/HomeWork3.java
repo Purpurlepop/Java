e952f1ba92b49d6fa0a279db7a7d50a34961db06
TSaveleva <nta_93@inbox.ru>
2024-07-19 15:01:01 +0300

    Создание игры-угадайки

--- src/main/java/HomeWork3.java
+++ src/main/java/HomeWork3.java
@@ -1,2 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HomeWork3 {
+    public static void main(String[] args) {
+        String[] CardDeck = {"6", "7", "8", "9", "10", "В", "Д", "К", "Т"};
+        int randomItem = (int)Math.floor(Math.random() * CardDeck.length);
+        String randomItem2 = Integer.toString(randomItem);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Угадай, какую карту я загадал?" + "\n" +
+                Arrays.toString(CardDeck));
+        String userInput = sc.nextLine();
+        boolean result = randomItem2.equalsIgnoreCase(userInput);
+        System.out.println("Результат игры: " + result);
+
+
+    }
 }
